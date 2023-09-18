f1b61d567ae9e0ecf3a2773aa9e23e9476f112ee
NielsZw <66211233+NielsZw@users.noreply.github.com>
2023-09-18 14:27:33 +0200

    Update main.c (#7)
    send + ID werkt (weer)

--- sensormodules/main.c
+++ sensormodules/main.c
@@ -2,6 +2,7 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/pgmspace.h>
 #include "nrf24spiXM2.h"
 #include "nrf24L01.h"
 #include "serialF0.h"
@@ -15,9 +16,9 @@
 
 #define MY_ID 0x41 //A = 0x41, B = 0x42, C = 0x43 
 
-char pipe[5] = {'2','s','e','x','y'};
-char IDpipe[5] = {'s','e','x','y',MY_ID};
-
+uint8_t pipe[5] = {0x32,0x73,0x65,0x78,0x79}; // "2sexy"
+uint8_t IDpipe[5] = {0x73,0x65,0x78,0x79,MY_ID}; //"sexy + MY_ID"
+uint8_t TXpipe[5] = {0x73,0x65,0x78,0x79,0x42};    // "sexy + 41"
 
 //used functions
 void init_nrf(void);
@@ -69,8 +70,6 @@ void init_nrf(void)
 	PORTF.DIRSET = PIN0_bm;
 	nrfspiInit();                              // Initialize SPI
 	nrfBegin();                                // Initialize radio module
-	nrfSetRetries(NRF_SETUP_ARD_1000US_gc,     // Auto Retransmission Delay: 1000 us
-	NRF_SETUP_ARC_8RETRANSMIT_gc);			   // Auto Retransmission Count: 8 retries
 	nrfSetPALevel(NRF_RF_SETUP_PWR_6DBM_gc);   // Power Control: -6 dBm
 	nrfSetDataRate(NRF_RF_SETUP_RF_DR_250K_gc);// Data Rate: 250 Kbps
 	nrfSetCRCLength(NRF_CONFIG_CRC_16_gc);     // CRC Check
@@ -81,7 +80,7 @@ void init_nrf(void)
 	nrfFlushRx();                              // Flush fifo's
 	nrfFlushTx();
 	
-	nrfOpenWritingPipe((uint8_t *) pipe);                  // Pipe for sending
+	nrfOpenWritingPipe((uint8_t *) TXpipe);                  // Pipe for sending
 	nrfOpenReadingPipe(0, (uint8_t *) pipe);               // Necessary for acknowledge
 	nrfOpenReadingPipe(1, (uint8_t *) IDpipe);
 	nrfStartListening();
@@ -93,19 +92,19 @@ void send(char *command)
 	
 	size_t commandlength = strlen(command);
 	
-	char *data = (char *)malloc(commandlength + 2);
+	char data[commandlength + 2];
 	
 	data[0] = ID;
 	
 	strcpy(data + 1, command);
 	
 	nrfStopListening();  
-	nrfWrite((uint8_t *)data, sizeof(data));
+	nrfWrite((uint8_t *)data, strlen(data));
 	
 	printf("\nVerzonden:%s\n", data);
 	nrfStartListening(); 
 	
-	free(data);
+
 	
 }
 

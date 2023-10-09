8f90c7b4d4f501e0a4743838441bf595d9be4b52
MaxD1003 <64729103+MaxD1003@users.noreply.github.com>
2023-10-09 11:31:36 +0200

    Add files via upload

---
+++ sensormodules/encryption.h
@@ -0,0 +1,17 @@
+/*
+ * encryption.h
+ *
+ * Created: 6-10-2023 11:50:43
+ *  Author: HP
+ */ 
+
+
+#ifndef ENCRYPTION_H_
+#define ENCRYPTION_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void encrypt_decrypt(uint8_t *pData, uint8_t *pKey, size_t dataSize, size_t keySize);
+
+#endif /* ENCRYPTION_H_ */

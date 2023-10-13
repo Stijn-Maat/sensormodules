252796ea686fd07339e93811e02e97a174cefd3e
Stijn Maat <77274889+Stijn-Maat@users.noreply.github.com>
2023-10-13 16:36:03 +0200

    Update Neighbor.h

--- sensormodules/Neighbor.h
+++ sensormodules/Neighbor.h
@@ -1,26 +1,23 @@
-#ifndef NEIGHBORH
-#define NEIGHBORH
+#ifndef NEIGHBOR_H_
+#define NEIGHBOR_H_
 
 #include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-#include "serialF0.h"
 
-#define MY_ID 0x47 // A = 0x41, B = 0x42, C = 0x43
+#define FORGET_BUURMAN_TIME 20
+#define MAX_NEIGHBORS 255
+#define MAX_VIA_NEIGHBORS 20
+#define MAX_HOPS 16
 
 typedef struct {
 	uint8_t id;
-	uint8_t hops;
-	uint8_t next_neighbor_id;
+	uint8_t hopIndex[MAX_HOPS];
+	uint8_t via_neighbor_id[MAX_HOPS][MAX_VIA_NEIGHBORS];
 	uint8_t valid_time;
 } Neighbor_t;
 
 
-uint8_t* get_neighbors(uint8_t *id);
-void remove_neighbor(uint8_t id);
-void received_packet(uint8_t *packet, uint8_t length, uint8_t receivePipe);
 void add_neighbor(Neighbor_t newNeighbor);
-void cal_hops(uint8_t source_id, uint8_t neighbor_id, uint8_t destination_id);
-bool add_next_neighbor(uint8_t destination_id, uint8_t next_neighbor_id, Neighbor_t newNeighbor);
-
-#endif /* NEIGHBOR */
+void remove_neighbor(uint8_t id);
+void count_down(void);
+uint8_t calculated_routing(uint8_t destination);
+#endif /* NEIGHBOR_H_ */

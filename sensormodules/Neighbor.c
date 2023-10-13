c6f6f2e236007707334e66adc0989d4afdadd03c
Stijn Maat <77274889+Stijn-Maat@users.noreply.github.com>
2023-10-13 16:35:40 +0200

    Update Neighbor.c

--- sensormodules/Neighbor.c
+++ sensormodules/Neighbor.c
@@ -1,30 +1,99 @@
-#include "Neighbor.h"
-#include <stdbool.h>
-
-#define FORGET_BUURMAN_TIME 20
-#define MAX_NEIGHBORS 255
-#define MAX_HOPS 16
+#include "neighbor.h"
 
 Neighbor_t neighbors[MAX_NEIGHBORS];
 uint8_t numNeighbors = 0;
 
-uint8_t* get_neighbors(uint8_t *id) {
-	for (uint8_t i = 0; i < numNeighbors; i++) {
-		if (neighbors[i].id == *id) {
-			return &(neighbors[i].id);
+uint8_t new_neighbor(uint8_t broad_id, uint8_t i, uint8_t broad_nodes, uint8_t *buf) {
+	uint8_t	hops;
+	neighbors[i].id = broad_id;
+	neighbors[i].valid_time = FORGET_BUURMAN_TIME;
+	// statement for adding via_id to Via_Neighbor_list
+	for(uint8_t j=0;j<broad_nodes;j++){
+		hops = *(buf+1+j*2)
+		if(hops<MAX_HOPS){
+			if(neighbors[i].hopIndex[hops]<MAX_VIA_NEIGHBORS){
+				neighbors[i].via_neighbor_id[hops][neighbors.hopIndex[hops]]=*(buf+j*2);
+				neighbors[i].hopIndex[hops]++;
+			}
 		}
 	}
-	return NULL;
+	return 1;
 }
 
 
-void add_neighbor(Neighbor_t newNeighbor) {
+
+uint8_t get_snapshot(uint8_t *data, uint8_t *node_amount, uint8_t *hops_amount){
+	uint8_t  snap_index = 0;
+
+	for(uint8_t i = 0; i < numNeighbors; i++){
+		*(data+snap_index) = neighbors[i].id;
+		snap_index++;
+		
+		uint8_t hops_count = 0;
+		for (uint8_t j= 0; j < MAX_HOPS; j++){
+			for (uint8_t k = 0; k < MAX_VIA_NEIGHBORS; k++){
+				
+				if(neighbors[i].via_neighbor_id[j][k] != 0){
+					*(data+snap_index) = neighbors[i].via_neighbor_id[j][k];
+					snap_index++;
+					hops_count++;
+				}
+			}
+			*(hops_amount + i) = hops_count;
+		}
+			*node_amount = snap_index;
+	}
+	return 1;
+}
+
+void add_neighbor(uint8_t broad_id,uint8_t broad_nodes, uint8_t *buf) {
 	if (numNeighbors < MAX_NEIGHBORS) {
-		neighbors[numNeighbors++] = newNeighbor;
-		} 
-		else {
+		if (numNeighbors>0){
+			//loop for existing direct neighbors
+			for (uint8_t i=0;i<numNeighbors;i++){
+				//if broad id exists refresh via list and valid time
+				if (broad_id == neighbors[i].id){
+					if (new_neighbor(broad_id,i,broad_nodes,buf)){
+						return;
+					}
+					
+				}
+			}
+			new_neighbor(broad_id,numNeighbors,broad_nodes,buf);
+			return;	
+		}
+		new_neighbor(broad_id,numNeighbors,broad_nodes,buf);
+		return;
+	}
+	else {
 		printf("Max neighbors reached.\n");
 	}
+	
+}
+
+uint8_t calculated_routing(uint8_t destination){
+	uint8_t direct_id;
+	//loop through lists of neighbors
+	for (uint8_t i=0;i<numNeighbors;i++){
+		// if direct id == destination, return direct neighbor id
+		if (neighbors[i].id==destination){
+			direct_id = neighbors[i].id;
+			return direct_id;
+		}else{
+			// else loop via list of direct neighbor
+			for (uint8_t j=0;j<MAX_HOPS;j++){
+				for (uint8_t k=0;k<MAX_VIA_NEIGHBORS;k++){
+					// if via id == destination, return direct neighbor id
+					if (neighbors[i].via_neighbor_id[j][k]==destination){
+						direct_id = neighbors[i].id;
+						return direct_id;
+					}
+				}
+			}
+		}
+	}
+	// when not in list return 0
+	return 0;
 }
 
 void remove_neighbor(uint8_t id) {
@@ -39,70 +108,8 @@ void remove_neighbor(uint8_t id) {
 		}
 	}
 }
-//ontvangen en opdelen van het packet in packetbuffer
-void received_packet(uint8_t *packet, uint8_t length, uint8_t receivePipe) {
 
-	Neighbor_t newNeighbor;
-	newNeighbor.id = packet[0];
-	newNeighbor.hops = 0;
-	newNeighbor.next_neighbor_id = 0;
-	newNeighbor.valid_time = FORGET_BUURMAN_TIME;
-	// hiervoor get_neigbour toevoegen voor selecteren of buur al bestaat of niet
-	add_neighbor(newNeighbor);
-	return;
-}
-
-//goed idee moet alleen nog uitgewerkt worden
-
-/*bool add_next_neighbor(uint8_t destination_id, uint8_t next_neigbor_id, Neighbor_t newNeighbor){
-	uint8_t send = 1;
-	next_neigbor_id = newNeighbor.id;
-	
-	//Give the ID from the destination
-	printf("I want to send a message to: ");
-	fgets((char*) destination_id, sizeof(destination_id), stdin);
-
-	printf("You typed: %c", destination_id);
-	
-	//When destination received, request to the neighbor to send their neighborlist
-	if (destination_id == UART_NO_DATA){
-		nrfStopListening();
-		nrfWrite(send,sizeof(send));
-		nrfStartListening();
-	}
-	
-	//Send neighborlist when 1 is received
-	if (packetBuffer == 1){
-		nrfStopListening();
-		for (uint8_t i = 0; i < numNeighbors; i++){
-			nrfWrite(&neighbors[i].id, sizeof(neighbors[i].id));
-		}
-		nrfStartListening();
-	}
-	
-	//Saves next neighbor ID
-	if (packetBuffer != neighbors[0].id){
-		for (uint8_t i = 0; i < numNeighbors; i++) { // Iterate only over valid neighbors
-			if (neighbors[i].next_neighbor_id == next_neigbor_id) {
-				return true;
-			}
-		}
-	}
-	return false;
-}*/
-
-
-//printen burenlijst
-void print_neighbors() {
-	printf("Burenlijst:\n");
-	for (uint8_t i = 0; i < numNeighbors; i++) {
-		printf("ID: %02X, Hops: %d, Valid Time: %d, Next Neighbor ID: %02X\n",
-		neighbors[i].id, neighbors[i].hops, neighbors[i].valid_time, neighbors[i].next_neighbor_id);
-	}
-}
-
-// functie voor het aftellen van de valid_time wanneer id niet meer relevant is
-void count_down() {
+void count_down(void){
 	for (uint8_t i = FORGET_BUURMAN_TIME - 1; i >= 0; i--) {
 		if (neighbors[i].valid_time > 0) {
 			neighbors[i].valid_time--;
@@ -115,5 +122,3 @@ void count_down() {
 		}
 	}
 }
-
-

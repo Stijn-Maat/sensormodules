1f7c916728933ee37fe0835481ed1a5b283fde02
Stijn Maat <77274889+Stijn-Maat@users.noreply.github.com>
2023-10-20 17:51:55 +0200

    Update neighbor.c

--- neighbor.c
+++ neighbor.c
@@ -1,159 +1,159 @@
-#include "neighbor.h"
+#include "Neighbor.h"
+#include <stdio.h>
+#include "core.h"
 
-// Define of Maximum number of neighbors
-Neighbor_t neighbors[MAX_NEIGHBORS];
+// Define of Maximum number of neighbors and Via Neighbors
+static Neighbor_t neighbors[MAX_NEIGHBORS];
+static Via_t vias[MAX_VIA_NEIGHBORS];
 
-// Score of the current number of neighbors
-uint8_t numNeighbors = 0;
+// Score of the current number of neighbors & vias
+static uint8_t numNeighbors = 0;
+static uint8_t numVias = 0;
 
-// Hop_counter
-volatile uint8_t hop_count = 0; // dynamisch maken
+// Static functions used only in c file
+static void update_via(uint8_t id, uint8_t hops, uint8_t host);
+static void remove_neighbor(uint8_t id);
+static void remove_host_vias(uint8_t host);
+static uint8_t update_neighbor_vias(uint8_t broad_id, uint8_t broad_nodes, uint8_t *buf);
 
 
-// Function to add a new Neighbor
-uint8_t new_neighbor(uint8_t broad_id, uint8_t i, uint8_t broad_nodes, uint8_t *buf) {
-	uint8_t	hops;
+uint8_t update_neighbor_vias(uint8_t broad_id, uint8_t broad_nodes, uint8_t *buf) {
 	
-	// set neigbhor ID and valid_time
-	neighbors[i].id = broad_id;
-	neighbors[i].valid_time = FORGET_BUURMAN_TIME;
-	// statement for adding via_id to Via_Neighbor_list
-	for(uint8_t j=0;j<broad_nodes;j++){
-		hops = *(buf+1+j*2);
-		if (hops>neighbors[i].hopCount){
-			neighbors[i].hopCount=hops;
-		}
-		if(hops<MAX_HOPS){
-			if(neighbors[i].hopIndex[hops]<MAX_VIA_NEIGHBORS){
-				neighbors[i].via_neighbor_id[hops][neighbors[i].hopIndex[hops]]=*(buf+j*2);
-				neighbors[i].hopIndex[hops]++;
-			}
-		}
+	// Updating via list
+	for(uint8_t i = 0; i < broad_nodes; i+=2){
+		printf("%d\n", i);
+		update_via(buf[i], buf[i+1] + 1, broad_id);
 	}
-	return 1;
+	return 0;
 }
 
-
-// Function to get a snapshot of neighbors
-uint8_t get_snapshot(uint8_t *data, uint8_t *node_amount){
-	uint8_t  snap_index = 0;
-	uint8_t node_count = 0;
+void update_via(uint8_t id, uint8_t hops, uint8_t host){
 	
-	// Add neighbors ID to snapshot
-	for(uint8_t i = 0; i < numNeighbors; i++){
-		*(data+snap_index) = neighbors[i].id;
-		snap_index++;
-		node_count++;
-		*(data+snap_index) = 0;
-		snap_index++;
-		
-		// Check snapshot sizelimit
-		if(snap_index ==29){
-			*node_amount = node_count;
-			return 1;
-		}
-		
-		// Kijken naar Defines dynamisch maken zodat er geen krap in komt
-		//Populate the snapshot with via_neighbors
-		for (uint8_t j= 0; j < neighbors[i].hopCount; j++){
-			for (uint8_t k = 0; k < neighbors[i].hopIndex[j]; k++){
-				
-				if(neighbors[i].via_neighbor_id[j][k] != 0){
-					*(data+snap_index) = neighbors[i].via_neighbor_id[j][k];
-					snap_index++;
-					*(data+snap_index) = j+1;
-					snap_index++;
-				}
-				// Check snapshot sizelimit
-				if(snap_index ==29){
-					*node_amount = node_count;
-					return 1;
-				}
+	for(uint8_t i = 0; i < numVias; i++){
+		if(vias[i].id == id){
+			if(vias[i].hops >= hops){
+				vias[i].hops = hops;
+				vias[i].host = host;
 			}
-		}
-		
-	}
-	*node_amount = snap_index;
-	return 1;
-}
-
-// Function to add Neighbor
-void add_neighbor(uint8_t broad_id,uint8_t broad_nodes, uint8_t *buf) {
-	if (numNeighbors < MAX_NEIGHBORS) {
-		if (numNeighbors>0){
-			//loop for existing direct neighbors
-			for (uint8_t i=0;i<numNeighbors;i++){
-				//if broad id exists refresh via list and valid time
-				if (broad_id == neighbors[i].id){
-					if (new_neighbor(broad_id,i,broad_nodes,buf)){
-						return;
-					}
-					
-				}
-			}
-			new_neighbor(broad_id,numNeighbors,broad_nodes,buf);
 			return;
 		}
-		new_neighbor(broad_id,numNeighbors,broad_nodes,buf);
-		return;
-	}
-	else {
-		printf("Max neighbors reached.\n");
 	}
 	
+	vias[numVias].id = id;
+	vias[numVias].hops = hops;
+	vias[numVias].host = host;
+	printf("id: %x, hops: %d, host: %d\n",id, hops, host);
+	numVias++;
+}
+
+// Function to get a snapshot of neighbors
+uint8_t get_snapshot(uint8_t *data) {
+	uint8_t dataPingIndex = 2;
+	
+	data[0] = my_id;
+	data[1] = SNAPSHOT;
+	
+	for(uint8_t i = 0; i < numNeighbors; i++, dataPingIndex+=2){
+		data[dataPingIndex] = neighbors[i].id;
+		data[dataPingIndex+1] = 0;
+	}
+	for(uint8_t via = 0; via < numVias; via++){
+		for(uint8_t i = 0; i < numNeighbors; i++){
+			if (neighbors[i].id == vias[via].id){
+				break;
+			}
+			data[dataPingIndex] = vias[via].id;
+			data[dataPingIndex+1] = vias[via].hops;
+		}	
+	}
+	printf("Snapshot send");
+	return 1;
+}
+
+// Function to store Neighbor
+void add_neighbor(uint8_t broad_id, uint8_t broad_nodes, uint8_t *buf) {
+	
+	remove_host_vias(broad_id);
+	
+	if(numNeighbors >= MAX_NEIGHBORS){
+		printf("Max neighbors reached.\n");
+		return;	
+	}
+	
+	uint8_t place = 0;
+	
+	// Loop voor bestaande directe buren
+	for (; place < numNeighbors; place++) {
+		// Als het broadcast-ID bestaat, vernieuw dan de via-lijst en geldige tijd
+		if (broad_id == neighbors[place].id) {
+			break;
+		}
+	}
+	
+	neighbors[place].id = broad_id;
+	neighbors[place].valid_time = FORGET_BUURMAN_TIME;
+	numNeighbors++; // Verhoog het aantal buren na het toevoegen van een nieuwe buurman
+
+	// Als het broadcast-ID niet bestaat, voeg dan een nieuwe buurman toe
+	update_neighbor_vias(broad_id, broad_nodes, buf);
+	printf("Added new neighbor with ID: %d\n", broad_id);
 }
 
 // Function to calculate routing to a destination
-uint8_t calculated_routing(uint8_t destination){
-	uint8_t direct_id;
-	//loop through lists of neighbors
-	for (uint8_t i=0;i<numNeighbors;i++){
-		// if direct id == destination, return direct neighbor id
-		if (neighbors[i].id==destination){
-			direct_id = neighbors[i].id;
-			return direct_id;
-			}else{
-			// else loop via list of direct neighbor
-			for (uint8_t j=0;j<MAX_HOPS;j++){
-				for (uint8_t k=0;k<MAX_VIA_NEIGHBORS;k++){
-					// if via id == destination, return direct neighbor id
-					if (neighbors[i].via_neighbor_id[j][k]==destination){
-						direct_id = neighbors[i].id;
-						return direct_id;
-					}
-				}
-			}
+uint8_t calculated_routing(uint8_t destination) {
+      
+	for(uint8_t i = 0; i< numNeighbors; i++){
+		if(neighbors[i].id == destination){
+			printf("%d\n", destination);
+			return neighbors[i].id;
 		}
 	}
-	// when not in list return 0
+	
+	for(uint8_t i = 0; i < numVias; i++){
+		if (vias[i].id == destination){
+			printf("%d\n", destination);
+			return vias[i].id;
+		}
+	}
 	return 0;
 }
 
 // Function to remove a neighbor
 void remove_neighbor(uint8_t id) {
-	for (uint8_t i = 0; i < numNeighbors; i++) {
-		if (neighbors[i].id == id) {
-			// Shift elements to remove the neighbor
-			for (uint8_t j = i; j < numNeighbors - 1; j++) {
-				neighbors[j] = neighbors[j + 1];
+    for (uint8_t i = 0; i < numNeighbors; i++) {
+        if (neighbors[i].id == id) {
+            // Shift elements to remove the neighbor
+            for (uint8_t j = i; j < numNeighbors - 1; j++) {
+                neighbors[j] = neighbors[j + 1];
+            }
+            numNeighbors--;
+            printf("Neighbor removed: ID=%d\n", id);
+            break;
+        }
+    }
+}
+
+void remove_host_vias(uint8_t host){
+	
+	for (uint8_t i = 0; i < numVias; i++){
+		if(vias[i].host == host){
+			for(uint8_t j = i; j < numVias-1; j++){
+				vias[j] = vias[j+1];
 			}
-			numNeighbors--;
-			break;
+			numVias--;
 		}
 	}
 }
 
-// Function to count down and remove a neighbors with expired times
-void count_down(void){
-	for (uint8_t i = FORGET_BUURMAN_TIME - 1; i >= 0; i--) {
-		if (neighbors[i].valid_time > 0) {
-			neighbors[i].valid_time--;
-		}
+void count_down() {
+	for (uint8_t i = 0; i < numNeighbors; i++) {
+		neighbors[i].valid_time--;
 		if (neighbors[i].valid_time == 0) {
+			printf("Neighbor expired: ID=%d\n", neighbors[i].id);
 			remove_neighbor(neighbors[i].id);
-		}
-		if (neighbors[i].valid_time == FORGET_BUURMAN_TIME + 1) {
-			remove_neighbor(neighbors[i].id);
+			i--; // Decrement i as the array size has changed
+			numNeighbors--;
 		}
 	}
 }
+

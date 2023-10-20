627e7a9652fdb07f74d1a73eb969900743bc0bd0
Stijn Maat <77274889+Stijn-Maat@users.noreply.github.com>
2023-10-20 17:52:35 +0200

    Update neighbor.h

--- neighbor.h
+++ neighbor.h
@@ -4,23 +4,28 @@
 #include <stdio.h>
 
 #define FORGET_BUURMAN_TIME 20
-#define MAX_NEIGHBORS 30
-#define MAX_VIA_NEIGHBORS 20
-#define MAX_HOPS 16
+#define MAX_NEIGHBORS 253
+#define MAX_VIA_NEIGHBORS 253
+#define MAX_HOPS (vias/vias[0] + 2)
+#define SNAPSHOT 0 
+#define UPDATE 1
 
 typedef struct {
 	uint8_t id;
-	uint8_t hopCount;
-	uint8_t hopIndex[MAX_HOPS];
-	uint8_t via_neighbor_id[MAX_HOPS][MAX_VIA_NEIGHBORS];
+	uint8_t host;
+	uint8_t hops;
+} Via_t;
+
+typedef struct{
+	uint8_t id;
 	uint8_t valid_time;
 } Neighbor_t;
 
 
+
 void add_neighbor(uint8_t broad_id,uint8_t broad_nodes, uint8_t *buf);
-uint8_t new_neighbor(uint8_t broad_id, uint8_t i, uint8_t broad_nodes, uint8_t *buf);
-uint8_t get_snapshot(uint8_t *data, uint8_t *node_amount);
-void remove_neighbor(uint8_t id);
-void count_down(void);
+uint8_t get_snapshot(uint8_t *data);
 uint8_t calculated_routing(uint8_t destination);
+void count_down();
+
 #endif /* NEIGHBOR_H_ */
